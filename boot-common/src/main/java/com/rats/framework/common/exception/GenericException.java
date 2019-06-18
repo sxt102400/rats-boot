437726ee97a4f06cb0b00c15a55f23d2a7496ce6
sxt102400 <sxt102400@163.com>
2019-06-19 01:44:55 +0800

    2 init

---
+++ boot-common/src/main/java/com/rats/framework/common/exception/GenericException.java
@@ -0,0 +1,48 @@
+package com.rats.framework.common.exception;
+
+import java.io.Serializable;
+
+/**
+ * @author yafei
+ * @since 2016/11/15
+ */
+public class GenericException extends RuntimeException implements Serializable {
+    String errorCode;
+    String errorMessage;
+    Exception errOri;
+
+    public String getMessage() {
+        return this.getErrorMessage();
+    }
+
+    public GenericException() {
+    }
+
+    public GenericException(String errCode) {
+        this.errorCode = errCode;
+    }
+
+    public String getErrorCode() {
+        return this.errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMessage() {
+        return this.errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public Exception getErrOri() {
+        return this.errOri;
+    }
+
+    public void setErrOri(Exception errOri) {
+        this.errOri = errOri;
+    }
+}
